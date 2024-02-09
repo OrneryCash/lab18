60b3b9860745663561013cce52e706dbed0b8658
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2024-02-09 16:18:55 +0700

    lab18_1.cpp completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect a, Rect b){
+  double ix = min(a.x+a.w, b.x+b.w) - max(a.x, b.x);
+  double iy = min(a.y, b.y) - max(a.y-a.h,b.y-b.h);
+  if (min(ix, iy) > 0) return ix * iy;
+  return 0;
+}
 
-
-
+int main() {
+  Rect R1 = {1,1,5,5};
+  Rect R2 = {7,2,3,3};
+  overlap(R1, R2);
 }
